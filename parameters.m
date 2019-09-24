be28f336f8fd5618a9ba70e58c6afabc529b25de
Adrianordp <adrianordp@alu.ufc.br>
2019-09-24 17:33:46 -0300

    Conntrolador do exemplo 2.1 implementado com sucesso no simulink

--- parameters.m
+++ parameters.m
@@ -5,3 +5,16 @@ clc, clear all, close all
 h = 2;
 Ts = 0.01;
 Tsim = 4;
+s = tf('s');
+A = [0 0
+     1 1];
+F = [-4*exp(h)+1 -4*exp(h)];
+B = [1
+     0];
+I = eye(size(A));
+
+S = inv(s*I-A)
+S(1,1) = 1/s;
+S(1,2) = 0;
+S(2,1) = 1/s/(s-1);
+S(2,2) = 1/(s-1);
