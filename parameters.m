86ddb17fdc7369468a02c6703a0ecb06bdb22206
Adrianordp <adrianordp@alu.ufc.br>
2019-09-25 11:48:25 -0300

    Planta em espaço de estados, controlador em blocos baseado em Watanabe (1981)

--- parameters.m
+++ parameters.m
@@ -2,9 +2,9 @@
 % Parameter for example 2.1 from Manitius 1979
 clc, clear all, close all
 
-h = 2;
+h = 1;
 Ts = 0.01;
-Tsim = 4;
+Tsim = 10;
 s = tf('s');
 A = [0 0
      1 1];
@@ -13,8 +13,8 @@ B = [1
      0];
 I = eye(size(A));
 
-S = inv(s*I-A)
-S(1,1) = 1/s;
-S(1,2) = 0;
-S(2,1) = 1/s/(s-1);
-S(2,2) = 1/(s-1);
+S = inv(s*I-A);
+% S(1,1) = 1/s;
+% S(1,2) = 0;
+% S(2,1) = 1/s/(s-1);
+% S(2,2) = 1/(s-1);
