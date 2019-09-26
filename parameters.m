436157b31e09426d6df705c139b5475817654f2d
Adriano Controle <adriano@alu.ufc.br>
2019-09-26 15:19:03 -0300

    Topologia do controlador pela eq (9) do Watanabe (1981)

--- parameters.m
+++ parameters.m
@@ -1,43 +1,30 @@
 % parameters.m
 % Parameter for example 2.1 from Manitius 1979
+% 
+% To run this model, the step-size must be very small!
+
+% Clear cache
 clc, clear all, close all
 
-h = 1;
-Ts = 0.01;
-Tsim = 25;
+% Laplace variable
 s = tf('s');
+
+% Simulation time [s]
+Tsim = 15;
+
+% Initial conditions
+IC = [.1  0];
+
+% Delay [s]
+h = 1;
+
+% Dynamic matrix
 A = [0 0
      1 1];
-% A = [-10   0
-%        1 -10];
-% F = [(18-81*h)*exp(-10*h) -81*exp(-10*h)];
-F = [-4*exp(h)+1  -4*exp(h)];
+
+% Input matrix
 B = [1
      0];
-% BA = [1
-%       exp(-h)-1];
-I = eye(size(A));
-IC = [0 .10];
 
-S = inv(s*I-A);
-S(1,1) = 1/s;
-S(1,2) = 0;
-S(2,1) = 1/s/(s-1);
-S(2,2) = 1/(s-1);
-
-% G = (s*I-A)\B*exp(-s*h)
-% step(G,Tsim)
-% %%
-% clc
-% syms h theta lambda f1 f2
-% F = [-4*exp(h)+1  -4*exp(h)];
-% e1 = expm(-h*A);
-% e2 = expm(-theta*A);
-% B1 = [1
-%       0];
-% BA = e1*B1;
-% R = F*e1*e2*B1;
-% simplify(R);
-% 
-% R2 = det(lambda*eye(2)-A-BA*[f1 f2]);
-% collect(simplify(R2))
+% Controller gain
+F = [-4*exp(h)+1  -4*exp(h)];
