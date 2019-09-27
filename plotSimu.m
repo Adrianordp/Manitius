087b3093f4413756bf95f1dd06833350e60c1471
Adrianordp <adrianordp@alu.ufc.br>
2019-09-27 16:51:21 -0300

    Figuras com nomes nos títulos das janelas

--- plotSimu.m
+++ plotSimu.m
@@ -5,23 +5,23 @@
 close all
 
 % Plot y(t)
-figure                     % Initialize figure frame
-plot(t,y,'LineWidth',2)    % Plot curves
-grid on                    % Enable grid
-axis([min(t) max(t) -3 1]) % Limit axes
+figure('Name','y(t)','NumberTitle','off') % Initialize figure frame
+plot(t,y,'LineWidth',2)                   % Plot curves
+grid on                                   % Enable grid
+axis([min(t) max(t) -3 1])                % Limit axes
 % Name labels
 xlabel('Time [sample]')
 ylabel('y(t)')
 title('y(t)')
-hold on
-plot(t+1,-(2.856+.7)*exp(-2*t)+0.7)
-legend('y(t)','Dinâmica (\lambda+1)^2')
+% hold on
+% plot(t+1,-(2.856+.7)*exp(-2*t)+0.7)
+% legend('y(t)','Dinâmica (\lambda+1)^2')
+legend('y(t)')
 
 % Plot u(t)
-figure                      % Initialize figure frame
-plot(t,u,'LineWidth',2)     % Plot curves
-grid on                     % Enable grid
-% axis([min(t) max(t) -1 .2]) % Limit axes
+figure('Name','u(t)','NumberTitle','off') % Initialize figure frame
+plot(t,u,'LineWidth',2)                   % Plot curves
+grid on                                   % Enable grid
 % Name labels
 xlabel('Time [sample]')
 ylabel('u(t)')
@@ -29,10 +29,9 @@ title('u(t)')
 legend('u(t)')
 
 % Plot x(t)
-figure                       % Initialize figure frame
-plot(t,x,'LineWidth',2)      % Plot curves
-grid on                      % Enable grid
-% axis([min(t) max(t) -.3 .3]) % Limit axes
+figure('Name','x(t)','NumberTitle','off') % Initialize figure frame
+plot(t,x,'LineWidth',2)                   % Plot curves
+grid on                                   % Enable grid
 % Name labels
 xlabel('Time [sample]')
 ylabel('x(t)')
@@ -43,6 +42,7 @@ legend('x_1(t)','x_2(t)')
 figure                       % Initialize figure frame
 plot(t,xMA,'LineWidth',2)    % Plot curves
 grid on                      % Enable grid
+% Name labels
 xlabel('Time [sample]')
 ylabel('xMA(t)')
 title('xMA(t)')
